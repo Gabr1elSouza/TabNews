8511eb70280bbf39f3dc672ea8052d8ff3e63be6
Gabriel Souza <gablgdario03@gmail.com>
2023-10-27 15:31:08 -0300

    Adiciona arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home(){
-return<h1>Carol esse é o meu primeiro site, se o seu amigo é lindo dê uma risadinha! 😂</h1>
+
+  return<h1>Carol esse é o meu primeiro site, se o seu amigo é lindo dê uma risadinha! 😂</h1>
+
 }
 
 export default Home
